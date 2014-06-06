94fd50916644f2d8156f12008b81bfdc9662749d
Goran Kljajic <gkljajic@gmail.com>
2014-06-06 11:05:35 +1000

    Updated classifications model

--- app/scripts/models/classifications-model.js
+++ app/scripts/models/classifications-model.js
@@ -3,13 +3,63 @@
 angular.module('hacktagApp')
   .value('classifications', [
     {
-      name: "Customer Facing",
-      classifications: "1,2,3",
+      name: "Office-Based",
+      classifications: "6251,6317,1216,6076,1210",
       style: "top: 100px;left: 400px;height: 300px;width: 300px;"
     },
     {
-      name: "Some other bubble",
-      classifications: "3,4,5",
+      name: "Number Crunching",
+      classifications: "1200,1203,1214",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Labour",
+      classifications: "1206,1225,6058,6092",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Creative",
+      classifications: "6008,6304,6263",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Technical",
+      classifications: "6281,1223,1209",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Helping Others",
+      classifications: "1211,6163",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Fitness",
+      classifications: "6246",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Working Outdoors",
+      classifications: "1206,6205",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Telephone Calling",
+      classifications: "1203",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Customer-Facing",
+      classifications: "1204,1212,6043",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Selling",
+      classifications: "1220,6362",
+      style: "top: 50px;left: 215px;"
+    },
+    {
+      name: "Teaching",
+      classifications: "6123",
       style: "top: 50px;left: 215px;"
     }
   ]
